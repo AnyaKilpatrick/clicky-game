01f477611fcdd54d05921b6cfe69323d5e708e89
Anya <hannakilpatrick@gmail.com>
2018-06-14 00:35:10 -0400

    reorganized files (each component has own folder)

--- src/components/Game.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Game.css";
-
-const Game = props => (
-    <img className="rounded gameImgs" alt={props.name} src={props.image}/>
-);
-
-export default Game;
--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Jumbotron.css";
+
 const Jumbotron = () => (
     <div className="jumbotron text-center" id="jumbotron">
-        <a id="start" class="btn btn-outline-warning" href="#game" role="button">Play</a>
+        <a id="start" className="btn btn-outline-warning" href="#game" role="button">Play</a>
         {/* <button id="start" type="button" href="#game" class="btn btn-info">Start</button> */}
         {/* <button id="start" className="col-s2 text-center"><a  href="#game">start</a></button> */}
         <img className="img-fluid" alt="logo" src="./images/nickelodeon.png"/>
