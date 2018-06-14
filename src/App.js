4cef16cd408cf1f3566797b866b5c6a25093f42d
Anya <hannakilpatrick@gmail.com>
2018-06-14 17:36:22 -0400

    update array and score depending on whether image was clicked before or not

--- src/App.js
+++ src/App.js
@@ -17,9 +17,8 @@ class App extends React.Component {
     topScore:0
   };
 
-  // Randomize array element order in-place.
-  // Using Durstenfeld shuffle algorithm.
-  shuffleArray =()=>{
+  
+  shuffleArray =()=>{ // Randomize array element order in-place. Using Durstenfeld shuffle algorithm.
     let tempArr = this.state.imgArr;
     for (let i =  tempArr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -30,21 +29,38 @@ class App extends React.Component {
     this.setState({imgArr:tempArr});
   }
 
-  // updating score and topScore every time we click image
-  handleIncrement =(event)=>{
-    const newScore = this.state.score + 1;
-    // increment score by 1 each time you click on image
-    this.setState({score: newScore});
-    // if score is higher then topScore then update topScore
-    if(newScore > this.state.topScore){
-      this.setState({topScore:newScore});
+  
+  handleIncrement =(event)=>{ // updating score and topScore every time we click image
+    const targetIndex = event.target.id;//grab target's id
+
+    if(this.state.imgArr[targetIndex].clicked === false){ //if this image wasn't clicjed before
+      const newScore = this.state.score + 1;
+      // increment score by 1 each time you click on image
+      this.setState({score: newScore});
+      // if score is higher then topScore then update topScore
+      if(newScore > this.state.topScore){
+        this.setState({topScore:newScore});
+      }
+      
+      this.clickedImage(targetIndex); //passing id as an arument to the next function
+    }else{ //if this image was clicked before
+      alert("Oops");
+      this.setState({score:0});//set score back to 0
+
+      const tempArr = this.state.imgArr;
+      for(let i=0; i<tempArr.length;i++){
+        tempArr[i].clicked = false;
+      }
+      this.setState({imgArr: tempArr}); //setting all images' property "clicked" back to false;
+
+      console.log(JSON.stringify(this.state.imgArr));
+      this.shuffleArray();
     }
-    const target = event.target.id;//before shuffling we grab target's id
-    this.clickedImage(target); //passing id as an arument to the next function
+    
   }
 
-  //update img (set "clikced" to true)
-  clickedImage = (target) => {
+  
+  clickedImage = (target) => { //update img (set "clikced" to true)
     const tempArr = this.state.imgArr;
     tempArr[target].clicked = true; //setting target's property "clicked" to true;
     this.setState({imgArr: tempArr}); //updating imgArr
