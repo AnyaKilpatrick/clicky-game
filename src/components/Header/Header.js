fb358ec27901b0f724d47b15f6396570b196dcad
Anya <hannakilpatrick@gmail.com>
2018-06-15 02:02:29 -0400

    responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,9 @@ import "./Header.css";
 const Header = props => (
     <div className="container-fluid" id="header">
         <div className="row pt-3">
-            <h1 className="col">Clicky Game</h1>
-            <h2 className="col text-right">Score: {props.score} | Top Score: {props.topScore}</h2>
+            <h1 className="col-sm-12 col-md-6 col-lg-4">Clicky Game</h1>
+            <h2 className="col-sm-12 col-md-6 col-lg-4 text-center">{props.pTag}</h2>
+            <h2 className="col-sm-12 col-md-12 col-lg-4 text-right">Score: {props.score} | Top Score: {props.topScore}</h2>
         </div>
     </div>
 );
